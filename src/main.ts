b4887efd1c5c85773573ecd9a18b1214b336eb82
Anik Hossain <anik.wdev@gmail.com>
2025-08-16 15:39:37 +0600

    `Update CORS configuration to include new origin`

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://task-frontend-77ua.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
