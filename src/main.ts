87b3a968ea154bdbb9529896b96264db1637d701
Anik Hossain <anik.wdev@gmail.com>
2025-08-16 16:19:45 +0600

    `Update CORS origin in main.ts to use new frontend URL`

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://task-frontend-h3rg.onrender.com'],
+    origin: ['http://localhost:5173', 'https://task-management26.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
